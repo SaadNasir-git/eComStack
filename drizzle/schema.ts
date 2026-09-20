c1d2a3b5c6559b049692c519767ac64d47772214
Saad Nasir <fahadnasir829@gmail.com>
2026-09-20 17:59:43 +0500

    Make users tables

---
+++ drizzle/schema.ts
@@ -0,0 +1,55 @@
+import { defineRelations } from "drizzle-orm";
+import { pgTable, uuid, timestamp, boolean, pgEnum, text, varchar } from "drizzle-orm/pg-core";
+
+export const RoleEnum = pgEnum("userRole", ["user", "admin"]);
+
+export const users = pgTable("users", {
+    id: uuid().primaryKey().defaultRandom(),
+    name: text().notNull(),
+    email: text().notNull().unique(),
+    passwordHash: text(),
+    role: RoleEnum().default("user"),
+    isVerified: boolean().default(false),
+    createdAt: timestamp("created_at", { mode: "date" }).defaultNow().notNull(),
+    updatedAt: timestamp("updated_at", { mode: "date" }).$onUpdate(() => new Date())
+});
+
+export const userDevices = pgTable("userDevices", {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: varchar({ length: 36 }).notNull().references(() => users.id, { onDelete: "cascade" }),
+    refreshToken: text(),
+    lastUse: timestamp({ mode: "date" }).$onUpdate(() => new Date())
+});
+
+export const userDetails = pgTable("userDetails", {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: varchar({ length: 36 }).notNull().references(() => users.id, { onDelete: "cascade" }),
+    phone: text().notNull(),
+    address: text().notNull()
+});
+
+export const relations = defineRelations({ users, userDevices, userDetails }, (r) => ({
+    users: {
+        devices: r.many.userDevices({
+            from: r.users.id,
+            to: r.userDevices.userId,
+        }),
+        details: r.one.userDetails({
+            from: r.users.id,
+            to: r.userDetails.userId,
+        }),
+    },
+    userDevices: {
+        user: r.one.users({
+            from: r.userDevices.userId,
+            to: r.users.id,
+        }),
+    },
+    userDetails: {
+        user: r.one.users({
+            from: r.userDetails.userId,
+            to: r.users.id,
+        }),
+    },
+}),
+);
